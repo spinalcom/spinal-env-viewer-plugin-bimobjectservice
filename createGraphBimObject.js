5c2d4be54e99e08dfc37d2e4544ed1be7bf6b72a
waiss67 <baqui.waiss@gmail.com>
2018-12-10 17:27:20 +0100

    update and correct bug it work now

--- createGraphBimObject.js
+++ createGraphBimObject.js
@@ -10,21 +10,17 @@ let bimObjectService = {
   graph: null,
   context: null,
   getGraph() {
-    console.log("GET GRAPH")
     if (this.graph === null) {
       this.graph = SpinalGraphService.getGraph();
     }
-    console.log(this.graph)
     return this.graph;
   },
   async getContext() {
-    console.log("GET CONTEXT")
     if (this.context === null) {
       this.context = await SpinalGraphService.getContext('BIMObjectContext');
       if (typeof this.context === 'undefined') {
         this.context = await SpinalGraphService.addContext(
           'BIMObjectContext');
-        console.log(this.context);
         return this.context
       }
       return this.context
@@ -34,8 +30,6 @@ let bimObjectService = {
 
   async createBIMObject(dbid, name) {
     let myBIMObjNode = await this.getBIMObject(dbid);
-    console.log("CREATE BIM OBJECT")
-    console.log("ici le bim object n'est pas créer")
     if (typeof myBIMObjNode === 'undefined') {
       let myBIMObj = new SpinalBIMObject(dbid, name);
       myBIMObjNode = SpinalGraphService.createNode({
@@ -45,10 +39,7 @@ let bimObjectService = {
         },
         myBIMObj
       );
-      console.log("ici ce trouve l'id du node créer par le service graph")
-      console.log(myBIMObjNode);
       let BIMObjectContext = await this.getContext();
-      console.log(BIMObjectContext)
       await SpinalGraphService.addChildInContext(
         BIMObjectContext.info.id.get(),
         myBIMObjNode,
@@ -64,13 +55,10 @@ let bimObjectService = {
   },
   async getBIMObject(dbid) {
     let BIMObjectContext = await this.getContext();
-    console.log("GET BIM OBJECT")
-    console.log("ici le context exist")
     if (BIMObjectContext != undefined) {
       let BIMObjectArray = await BIMObjectContext.getChildren([
         'hasBIMObject',
       ]);
-      console.log(BIMObjectArray)
       for (let i = 0; i < BIMObjectArray.length; i++) {
         const element = BIMObjectArray[i];
         if (element.info.dbid.get() === dbid) {
